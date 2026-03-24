3862b72c65855261ece846379482841c8defbcdc
LouisaQvQ <louisafrancious1@gmail.com>
2026-03-23 21:29:14 -0400

    add discover page, profile page, and bottom nav

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -7,21 +6,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
->>>>>>> louisa
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
 );
 
 reportWebVitals();
-=======
-);
->>>>>>> louisa
