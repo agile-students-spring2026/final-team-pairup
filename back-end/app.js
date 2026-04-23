2cddcd18d4c2b09523122925dd981208908ab0b1
jk8308-jpg <jk8308@nyu.edu>
2026-04-22 23:47:14 -0400

    replace mock data with MongoDB for proposals and meetings

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,11 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err))
 
 const authStub = require("./middleware/authStub");
 const authRoutes = require("./routes/authRoutes");
